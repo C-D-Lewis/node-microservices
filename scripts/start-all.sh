a504832d4c842b32caf569cb076fb42292d47be7
C-D-Lewis <Bonsitm@gmail.com>
2018-10-28 15:26:50 +0000

    Add package files

---
+++ scripts/start-all.sh
@@ -0,0 +1,18 @@
+cd "../apps/conduit" && npm start &
+sleep 5
+cd "../apps/attic" && npm start &
+sleep 5
+cd "../apps/led-server" && npm start &
+sleep 5
+cd "../apps/backlight-server" && npm start &
+sleep 5
+cd "../apps/plug-server" && npm start &
+sleep 5
+cd "../apps/webhooks" && npm start &
+sleep 5
+cd "../apps/spotify-auth" && npm start &
+sleep 5
+cd "../apps/monitor" && npm start &
+sleep 5
+cd "../apps/service-dashboard" && npm run serve &
+sleep 5
