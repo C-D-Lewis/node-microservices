b311d9fb2622c414f51495363240f34e11b59132
C-D-Lewis <Bonsitm@gmail.com>
2018-11-27 19:28:35 +0000

    Initial files for dsn app

---
+++ apps/dsn/src/index.js
@@ -0,0 +1,73 @@
+const { parseString } = require('xml2js');
+const { promisify } = require('util');
+const { conduit, log } = require('@chris-lewis/node-common')(['conduit', 'log']);
+const request = require('request');
+
+const parseStringAsync = promisify(parseString);
+const requestAsync = promisify(request);
+
+const DSN_FEED_URL = 'https://eyes.nasa.gov/dsn/data/dsn.xml';
+const LED_COLORS = {
+  none: [0, 0, 0],
+  down: [0, 0, 100],
+  up: [153, 204, 255],
+};
+const INTERVAL_MS = 5000;
+
+const getNonce = () => Math.floor(new Date().getTime() / 5000);
+
+/**
+ * Unpick the XML-ness of the data
+ */
+const digest = (json) => {
+  const feed = {
+    stations: json.dsn.station.map(p => p.$),
+    timestamp: json.dsn.timestamp[0],
+  };
+  feed.dishes = json.dsn.dish.map((dish) => ({
+    metadata: dish.$,
+    downSignals: dish.downSignal ? dish.downSignal.map(p => p.$) : [],
+    upSignals: dish.upSignal ? dish.upSignal.map(p => p.$) : [],
+    targets: dish.target ? dish.target.map(p => p.$) : [],
+  }));
+
+  return feed;
+};
+
+const getLinkStates = feed => feed.dishes.reduce((res, item) => {
+  if (item.downSignals.some(p => p.signalType !== 'none')) {
+    return res.concat('down');
+  }
+
+  if (item.upSignals.some(p => p.signalType !== 'none')) {
+    return res.concat('up');
+  }
+
+  return res.concat('none');
+}, []);
+
+const update = async () => {
+  const { body } = await requestAsync(`${DSN_FEED_URL}?r=${getNonce()}`);
+  const json = await parseStringAsync(body);
+  const feed = digest(json);
+
+  const ledStates = getLinkStates(feed);
+  log.info(ledStates);
+  
+  const promises = ledStates
+    .map(item => LED_COLORS[item])
+    .map((rgb, i) => {
+      const packet = { to: 'LedServer', topic: 'setPixel', message: { [i]: rgb } };
+      return conduit.send(packet);
+    });
+  await Promise.all(promises);
+};
+
+const main = async () => {
+  log.begin();
+  await conduit.register();
+
+  setInterval(update, INTERVAL_MS);
+};
+
+main();
