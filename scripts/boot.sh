006e1688cb3ca63994839984a9bf116521d07d26
Chris Lewis <Bonsitm@gmail.com>
2018-11-28 20:55:01 +0000

    Simplify runner

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -4,6 +4,5 @@
 # Wait for network up
 /home/pi/node-microservices/scripts/wait_for_network.sh
 
-# Specify names of apps to launch as args
-cd /home/pi/node-microservices/runner && sudo npm start \
-  conduit attic &
+# Specify names of apps to launch as args in apps.txt
+cd /home/pi/node-microservices/runner && sudo npm start $(cat apps.txt) &
