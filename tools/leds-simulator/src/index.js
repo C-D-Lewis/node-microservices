b3cc65dc384b39feac1cf3151b31d8cf6f04b116
C-D-Lewis <Bonsitm@gmail.com>
2018-12-01 23:24:34 +0000

    Add leds-simulator

---
+++ tools/leds-simulator/src/index.js
@@ -0,0 +1,26 @@
+const { conduit } = require('@chris-lewis/node-common')(['conduit']);
+const chalk = require('chalk');
+
+const INTERVAL_MS = 500;
+
+const updateDisplay = (ledArr) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  ledArr.forEach((led) => {
+    const [r, g, b] = led;
+    process.stdout.write(chalk.rgb(r, g, b)('\u25FC '));
+  });
+};
+
+const main = async () => {
+  await conduit.register();
+
+  setInterval(async () => {
+    const packet = { to: 'LedServer', topic: 'state', message: {}};
+    const state = await conduit.send(packet);
+
+    updateDisplay(state.message.leds);
+  }, INTERVAL_MS);
+};
+
+main();
