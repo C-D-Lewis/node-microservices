91286da5bd86995ea561db4769ca71cb35849b7d
C-D-Lewis <Bonsitm@gmail.com>
2018-12-01 12:49:02 +0000

    Logging for schema error origins

--- node-common/src/modules/schema.js
+++ node-common/src/modules/schema.js
@@ -5,7 +5,7 @@ module.exports = (instance, schema) => {
   if(!(results.errors && results.errors.length > 0)) return true;
 
   results.errors.forEach(item => console.log(`schema error: ${item.stack}`));
-  console.log(`expected: ${JSON.stringiify(schema)}`);
-  console.log(`  actual: ${JSON.stringiify(instance)}`);
+  console.log(`expected: ${JSON.stringify(schema)}`);
+  console.log(`  actual: ${JSON.stringify(instance)}`);
   return false;
 };
