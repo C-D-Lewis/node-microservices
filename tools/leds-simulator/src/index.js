fd39e6b3bc4d155b6a2ff09a98402b2bfcfc3bf1
C-D-Lewis <Bonsitm@gmail.com>
2018-12-02 15:17:07 +0000

    Improve leds-simulator

--- tools/leds-simulator/src/index.js
+++ tools/leds-simulator/src/index.js
@@ -6,19 +6,14 @@ const INTERVAL_MS = 500;
 const updateDisplay = (ledArr) => {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
-  ledArr.forEach((led) => {
-    const [r, g, b] = led;
-    process.stdout.write(chalk.rgb(r, g, b)('\u25C9 '));
-  });
+  ledArr.forEach(led => process.stdout.write(chalk.rgb(...led)('\u25C9 ')));
 };
 
 const main = async () => {
   await conduit.register();
 
   setInterval(async () => {
-    const packet = { to: 'LedServer', topic: 'state', message: {}};
-    const state = await conduit.send(packet);
-
+    const state = await conduit.send({ to: 'LedServer', topic: 'state', message: {}});
     updateDisplay(state.message.leds);
   }, INTERVAL_MS);
 };
