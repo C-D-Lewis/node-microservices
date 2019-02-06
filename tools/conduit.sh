32dcff4eb673df2c4a1b01e02da3c81ba884ac8c
Chris Lewis <Bonsitm@gmail.com>
2019-02-06 22:08:23 +0000

    Add example to tool

--- tools/conduit.sh
+++ tools/conduit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Example:
+#  ./conduit.sh 192.168.1.107 BacklightServer set '{"all": [128,128,128]}'
+
 PORT=5959
 MESSAGE=${4:-"{}"}
 PACKET="{\"to\":\"$2\",\"topic\":\"$3\",\"message\":$MESSAGE}"
