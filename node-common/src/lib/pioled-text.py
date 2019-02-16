e492d7904e91e7476a85775a8666eac225c0ffab
Chris Lewis <Bonsitm@gmail.com>
2019-02-16 11:35:57 +0000

    Attempt parameters

--- node-common/src/lib/pioled-text.py
+++ node-common/src/lib/pioled-text.py
@@ -15,7 +15,6 @@ test: sudo python examples/stats.py
 '''
 
 # TODO: Parameterise this to allow multiple clients
-# TODO: Parameterise which of 4 lines to use
 # TODO: Parameterise clear at start
 
 import time
@@ -25,6 +24,12 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import subprocess
+
+# Parameters
+# python pioled-text.py "Hello, world!" 0 false
+text = int(sys.argv[1])
+line_number = int(sys.argv[2])
+clear_all = int(sys.argv[3])
 
 # 128x32 display with hardware I2C:
 RST = None
@@ -39,17 +44,11 @@ image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 
-cmd = "hostname -I | cut -d\' \' -f1"
-IP = subprocess.check_output(cmd, shell = True)
-cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-CPU = subprocess.check_output(cmd, shell = True)
-
 # Draw something
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
 x = 0
 top = 0
-draw.text((x, top), "IP: " + str(IP),  font=font, fill=255)
-draw.text((x, top + 8), str(CPU), font=font, fill=255)
+draw.text((x, top + (8 * line_number)), str(text), font=font, fill=255)
 
 # Display image.
 disp.image(image)
