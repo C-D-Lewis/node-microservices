1a00d489405778b152504cef70c040f877a3e781
C-D-Lewis <Bonsitm@gmail.com>
2019-02-17 12:40:32 +0000

    Update services.js

--- apps/monitor/src/plugins/services.js
+++ apps/monitor/src/plugins/services.js
@@ -26,32 +26,40 @@ config.requireKeys('services.js', {
 let savedState = true;
 
 module.exports = async (args) => {
-  if(sleep.sleeping()) return;
+  if (sleep.sleeping()) {
+    return;
+  }
 
   const host = args.HOST || 'localhost';
   log.debug(`services host=${host}`);
-  const { body } = await requestAsync(`http://${host}:${config.CONDUIT.PORT}/apps`);
-  const downApps = JSON.parse(body).reduce((result, item) => {
-    log.debug(`Service ${item.app} returned ${item.status}`);
-    if(item.status !== 'OK') result.push(item.app);
 
-    return result;
-  }, []);
+  const { body } = await requestAsync(`http://${host}:${config.CONDUIT.PORT}/apps`);
+  const downApps = JSON.parse(body)
+    .reduce((result, item) => {
+      log.debug(`Service ${item.app} returned ${item.status}`);
+      if (item.status !== 'OK') {
+        result.push(item.app);
+      }
+
+      return result;
+    }, []);
 
   const stateNow = downApps.length === 0;
-  log.debug(`stateNow=${stateNow}`);
+  log.info(`Services up: ${stateNow}`);
   await conduit.send({
     to: 'LedServer', topic: 'setPixel',
     message: { [args.LED]: stateNow ? config.LED_STATES.OK : config.LED_STATES.DOWN }
   });
 
   // Was OK, not anymore
-  if(savedState && !stateNow) {
+  if (savedState && !stateNow) {
     await fcm.post('Monitor', 'monitor', `Services not OK: ${downApps.join(', ')}`);
   }
 
   // Same as before
-  if(stateNow === savedState) return;
+  if (stateNow === savedState) {
+    return;
+  }
 
   // Changed, update
   savedState = stateNow;
