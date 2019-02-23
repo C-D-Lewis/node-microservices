01fb21f19e69b676c333502a98dd3ecd71fbee9d
C-D-Lewis <Bonsitm@gmail.com>
2019-02-23 17:03:44 +0000

    Add testing.js to replace testBed.js

---
+++ node-common/src/modules/testing.js
@@ -0,0 +1,23 @@
+const requestAsync = require('./requestAsync');
+
+module.exports = {
+  /**
+   * Return after specified number of milliseconds.
+   *
+   * @param {number} ms - Number of milliseconds.
+   * @returns {Promise} Promise that resolves when the time has elapsed.
+   */
+  sleepAsync: async ms => new Promise(resolve => setTimeout(resolve, ms)),
+
+  /**
+   * Send a Conduit packet.
+   *
+   * @param {object} json - The packet to send.
+   * @returns {Promise} Promise that resolves with the response.
+   */
+  sendConduitPacket: async json => requestAsync({
+    url: 'http://localhost:5959/conduit',
+    method: 'post',
+    json,
+  }).then(({ body }) => body),
+};
