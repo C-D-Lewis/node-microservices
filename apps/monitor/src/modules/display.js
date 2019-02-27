128c285a79c59d08fdfe01c6fa05378110142db1
Chris Lewis <Bonsitm@gmail.com>
2019-02-27 21:00:54 +0000

    Begin dual display format testing

--- apps/monitor/src/modules/display.js
+++ apps/monitor/src/modules/display.js
@@ -3,8 +3,8 @@
  */
 
 const {
-  conduit,
-} = require('@chris-lewis/node-common')(['conduit']);
+  conduit, config,
+} = require('@chris-lewis/node-common')(['conduit', 'config']);
 
 const setLed = async (index, rgb) => {
   return conduit.send({ to: 'LedServer', topic: 'setPixel', message: { [index]: rgb } });
@@ -18,8 +18,18 @@ const setText = async (lines, text) => {
   return conduit.send({ to: 'LedServer', topic: 'setText', message: { lines } });
 };
 
+const setBoth = (data) => {
+  const [ledData, textData] = data;
+  if (config.DISPLAY_DRIVER === 'textDisplay') {
+    return setText(textData.line, textData.text);
+  } else if (config.DISPLAY_DRIVER === 'leds') {
+    return setLed(ledData.index, ledData.rgb);
+  }
+};
+
 module.exports = {
   setLed,
   setAllLeds,
   setText,
+  setBoth,
 };
