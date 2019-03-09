5b229e9a17b43933ef2bdc441ec0714657371db8
C-D-Lewis <Bonsitm@gmail.com>
2019-03-09 22:31:50 +0000

    Rename BacklightServer to ambience

---
+++ apps/ambience/README.md
@@ -0,0 +1,15 @@
+# ambience
+
+Express Node.js server combined with
+[led-server](https://github.com/c-d-lewis/led-server) to make LED-based
+backlights connected and awesome.
+
+
+## API
+
+* `POST /set    { "to": [r, g, b] }`
+* `POST /fade   { "to": [r, g, b] }`
+* `POST /demo`
+* `POST /off`
+* `GET  /status`
+* `POST /spotify`
--- apps/ambience/tests/tests.js
+++ apps/ambience/tests/tests.js
@@ -7,7 +7,7 @@ const SLEEP_MS = 500;
 describe('API', () => {
   describe('Conduit topic: status', () => {
     it('should return 200 / OK', async () => {
-      const result = await testing.sendConduitPacket({ to: 'BacklightServer', topic: 'status' });
+      const result = await testing.sendConduitPacket({ to: 'ambience', topic: 'status' });
 
       expect(result.status).to.equal(200);
       expect(result.message.content).to.equal('OK');
@@ -16,7 +16,7 @@ describe('API', () => {
 
   describe('Conduit topic: demo', () => {
     it('should return 200 / OK', async () => {
-      const response = await testing.sendConduitPacket({ to: 'BacklightServer', topic: 'demo' });
+      const response = await testing.sendConduitPacket({ to: 'ambience', topic: 'demo' });
 
       expect(response.status).to.equal(200);
       expect(response.message.content).to.equal('OK');
@@ -27,7 +27,7 @@ describe('API', () => {
 
   describe('Conduit topic: off', () => {
     it('should return 200 / OK', async () => {
-      const response = await testing.sendConduitPacket({ to: 'BacklightServer', topic: 'off' });
+      const response = await testing.sendConduitPacket({ to: 'ambience', topic: 'off' });
 
       expect(response.status).to.equal(200);
       expect(response.message.content).to.equal('OK');
@@ -39,7 +39,7 @@ describe('API', () => {
   describe('Conduit topic: set', () => {
     it('should return 200 / OK', async () => {
       const response = await testing.sendConduitPacket({
-        to: 'BacklightServer', topic: 'set',
+        to: 'ambience', topic: 'set',
         message: { all: [64,64,64] }
       });
 
@@ -53,7 +53,7 @@ describe('API', () => {
   describe('Conduit topic: fade', () => {
     it('should return 200 / OK', async () => {
       const response = await testing.sendConduitPacket({
-        to: 'BacklightServer', topic: 'fade',
+        to: 'ambience', topic: 'fade',
         message: { all: [64,64,64] }
       });
 
@@ -66,7 +66,7 @@ describe('API', () => {
 
   describe('Conduit topic: spotify', () => {
     it('should return 200 / [r,g,b]', async () => {
-      const response = await testing.sendConduitPacket({ to: 'BacklightServer', topic: 'spotify' });
+      const response = await testing.sendConduitPacket({ to: 'ambience', topic: 'spotify' });
 
       expect(response.status).to.equal(200);
       expect(response.message.content).to.be.an('array');
