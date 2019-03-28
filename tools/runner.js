9d98a56b432459232ab938a6a6c781eced3e0710
Chris Lewis <Bonsitm@gmail.com>
2019-03-28 18:36:50 +0000

    Improve runner.js

--- tools/runner.js
+++ tools/runner.js
@@ -1,20 +1,13 @@
 const { spawn } = require('child_process');
 const fs = require('fs');
 
-const INTERVAL_MS = 10000;
-
-const children = {};
+const INTERVAL_S = 10;
 
 const waitAsync = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const queue = async (items) => {
-  if (!items.length) {
-    return;
-  }
-
-  const next = items.shift();
-  return next().then(() => queue(items));
-};
+const queue = async items => items.length
+    ? items.shift()().then(() => queue(items))
+    : null;
 
 const main = async () => {
   const appList = process.argv.slice(2);
@@ -23,10 +16,8 @@ const main = async () => {
       throw new Error(`App does not exist: ${app}`);
     }
 
-    console.log(`Spawning process for ${app}`);
-    children[app] = spawn(`cd ../apps/${app} && npm start`, { stdio: 'inherit', shell: true });
-
-    return waitAsync(INTERVAL_MS);
+    spawn(`cd ../apps/${app} && npm start`, { stdio: 'inherit', shell: true });
+    await waitAsync(INTERVAL_S * 1000);
   });
 
   await queue(tasks);
