d507d56e0435f0bef03caf8ae670867e06e0e22d
C-D-Lewis <Bonsitm@gmail.com>
2019-05-27 14:05:36 +0100

    Add motePhat server link

--- node-common/src/modules/motePhat.js
+++ node-common/src/modules/motePhat.js
@@ -1,5 +1,8 @@
 const { execSync } = require('child_process');
+const requestAsync = require('./requestAsync');
 const log = require('./log');
+
+const MOTE_PHAT_SERVER_POST = 35275;
 
 const setAll = (rgb) => {
   log.assert(Array.isArray(rgb), `rgb must be an array. Was ${rgb}`);
@@ -14,7 +17,24 @@ const setPixels = (leds) => {
   execSync(`python ${`${__dirname}/../lib/mote-phat-pixels.py`} ${JSON.stringify(leds)}`);
 };
 
+const setAllServer = async (rgb) => {
+  log.assert(Array.isArray(rgb), `rgb must be an array. Was ${rgb}`);
+
+  try {
+    return requestAsync({
+      url: `http://localhost:${MOTE_PHAT_SERVER_POST}/setall`,
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ all: rgb }),
+    });
+  } catch (e) {
+    log.error(e);
+    log.error('Is mote-phat-server.py running?');
+  }
+};
+
 module.exports = {
   setAll,
+  setAllServer,
   setPixels,
 };
