c549a90224f8512d25350674ef502e091f0d45d2
C-D-Lewis <Bonsitm@gmail.com>
2019-05-27 13:33:06 +0100

    Update tools/boot.sh

--- tools/boot.sh
+++ tools/boot.sh
@@ -5,4 +5,4 @@
 /home/pi/node-microservices/tools/wait_for_network.sh
 
 # Specify names of apps to launch as args in apps.txt
-sudo node /home/pi/node-microservices/tools/runner.js $(cat /home/pi/apps.txt) &
+cd /home/pi/node-microservices/tools && sudo node runner.js $(cat /home/pi/apps.txt) &
