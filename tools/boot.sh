ae6b1e1247b57fb0b1fb7fc626d06d016ba0ad3b
C-D-Lewis <Bonsitm@gmail.com>
2019-05-27 13:31:33 +0100

    Update tools/boot.sh

--- tools/boot.sh
+++ tools/boot.sh
@@ -2,7 +2,7 @@
 # This combination of shebang and absolute paths appears to work with crontab @reboot
 
 # Wait for network up
-/home/pi/node-microservices/scripts/wait_for_network.sh
+/home/pi/node-microservices/tools/wait_for_network.sh
 
 # Specify names of apps to launch as args in apps.txt
-sudo node /home/pi/node-microservices/runner.js $(cat /home/pi/apps.txt) &
+sudo node /home/pi/node-microservices/tools/runner.js $(cat /home/pi/apps.txt) &
