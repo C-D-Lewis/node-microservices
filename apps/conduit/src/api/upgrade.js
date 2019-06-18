25b440f6af1adae5f096ddf745d5b8ba0fb6ed3a
Chris Lewis <Bonsitm@gmail.com>
2019-06-18 21:10:06 +0100

    Handle upgrade failure, server always sets CORS

--- apps/conduit/src/api/upgrade.js
+++ apps/conduit/src/api/upgrade.js
@@ -3,15 +3,19 @@ const { execSync } = require('child_process');
 const DELAY_MS = 10000;
 
 module.exports = async (req, res) => {
-  // Git update
-  execSync('git checkout ../..');
-  execSync('git reset --hard HEAD');
-  execSync('git pull origin master');
+  try {
+    // Git update
+    execSync('git checkout ../..');
+    execSync('git reset --hard HEAD');
+    execSync('git pull origin master');
 
-  // Delayed reboot
-  setTimeout(() => execSync('sudo reboot'), DELAY_MS);  
+    // Delayed reboot
+    setTimeout(() => execSync('sudo reboot'), DELAY_MS);  
 
-  res.status(200);
-  res.set('Access-Control-Allow-Origin', '*');
-  res.json({ content: 'Restarting in 10 seconds' });
+    res.status(200);
+    res.json({ content: 'Restarting in 10 seconds' });
+  } catch (e) {
+    log.error(e);
+    res.status(500).
+  }
 };
