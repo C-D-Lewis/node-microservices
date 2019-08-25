d796dd58c5c8ee0afad4f67658c23912a99abaef
C-D-Lewis <Bonsitm@gmail.com>
2019-08-25 12:01:45 +0100

    Update .circleci/config.yml

--- tools/runner.js
+++ tools/runner.js
@@ -5,22 +5,18 @@ const INTERVAL_S = 10;
 
 const waitAsync = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const queue = async items => items.length
-    ? items.shift()().then(() => queue(items))
-    : null;
-
 const main = async () => {
   const appList = process.argv.slice(2);
-  const tasks = appList.map(app => async () => {
+  for (let app of appList) {
     if (!fs.existsSync(`../apps/${app}`)) {
       throw new Error(`App does not exist: ${app}`);
     }
 
-    spawn(`cd ../apps/${app} && npm start`, { stdio: 'inherit', shell: true });
+    const options = { stdio: 'ignore', shell: true, detatched: true };
+    spawn(`cd ../apps/${app} && npm start`, options).unref();
     await waitAsync(INTERVAL_S * 1000);
-  });
+  }
 
-  await queue(tasks);
   console.log(`\nRunning ${appList.join(', ')}`);
 };
 
