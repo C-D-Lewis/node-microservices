9c9eec1718de38396aca4ba2d58605d66eadd161
C-D-Lewis <Bonsitm@gmail.com>
2019-08-31 16:10:49 +0100

    JSDoc

--- apps/attic/src/modules/mongo.js
+++ apps/attic/src/modules/mongo.js
@@ -1,13 +1,19 @@
 const { log } = require('../node-common')(['log']);
 const { MongoClient } = require('mongodb');
 
+/** mongod URL **/
 const MONGO_DB_URL = 'mongodb://localhost:27017';
+/** MongoDB name **/
 const MONGO_DB_NAME = 'AtticAppData';
+/** MongoDB collection name **/
 const MONGO_DB_COLLECTION_NAME = 'AppDataDocument';
 
 let client = new MongoClient(MONGO_DB_URL);
 let db;
 
+/**
+ * Initialise
+ */
 const init = async () => {
   await client.connect();
   db = client.db(MONGO_DB_NAME);
@@ -15,15 +21,33 @@ const init = async () => {
   log.debug('Connected to mongodb');
 };
 
-const get = async (key) => {
+/**
+ * Get a value by key.
+ *
+ * @param {string} key - The key.
+ * @returns {*} The value.
+ */
+ const get = async (key) => {
   const res = await db.collection(MONGO_DB_COLLECTION_NAME).findOne({ key });
   log.debug({ res });
 
   return res ? res.value : res;
 };
 
+/**
+ * Check a key exists.
+ *
+ * @param {string} key - The key.
+ * @returns {boolean} true if the key exists.
+ */
 const exists = async key => get(key) !== null;
 
+/**
+ * Set a value with key and value.
+ *
+ * @param {string} key - The key.
+ * @param {string} value - The value.
+ */
 const set = async (key, value) => {
   const res = await db.collection(MONGO_DB_COLLECTION_NAME).findOneAndUpdate(
     { key },
