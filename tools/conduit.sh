5af3fdbab5ca9cd524d30779ba0a726edc92468e
C-D-Lewis <Bonsitm@gmail.com>
2019-10-05 21:08:51 +0100

    Fix show-fleet

--- tools/conduit.sh
+++ tools/conduit.sh
@@ -7,5 +7,5 @@ PORT=5959
 MESSAGE=${4:-"{}"}
 PACKET="{\"to\":\"$2\",\"topic\":\"$3\",\"message\":$MESSAGE}"
 
-curl -X POST $1:$PORT/conduit -H Content-Type:application/json -d "$PACKET"
+curl -X POST $1:$PORT/conduit -H Content-Type:application/json -d "$PACKET" -s
 echo ''
