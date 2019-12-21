39aac28bb31230b4822ccb5397ff27c72a4edcff
C-D-Lewis <Bonsitm@gmail.com>
2019-12-21 17:28:21 +0000

    Cleanup datalogger.js plugin

--- apps/monitor/src/plugins/datalogger.js
+++ apps/monitor/src/plugins/datalogger.js
@@ -3,23 +3,24 @@ const {
 } = require('../node-common')(['requestAsync', 'log', 'extract', 'attic']);
 
 module.exports = async (args) => {
-  const configured = args.URL && args.BEFORES && args.AFTER && args.ATTIC_KEY;
-  log.assert(configured, 'datalogger.js requires ARGS specified', true);
+  const { URL, BEFORES, AFTER, ATTIC_KEY } = args;
+  const configured = URL && BEFORES && AFTER && ATTIC_KEY;
+  log.assert(configured, 'datalogger.js requires some ARGS specified', true);
 
   try {
-    const { body } = await requestAsync({ url: args.URL });
-    const value = extract(body, args.BEFORES, args.AFTER).trim();
-    
-    let values = [];
-    if (await attic.exists(args.ATTIC_KEY)) {
-      values = await attic.get(args.ATTIC_KEY);
+    const { body } = await requestAsync({ url: URL });
+    const value = extract(body, BEFORES, AFTER).trim();
+
+    let history = [];
+    if (await attic.exists(ATTIC_KEY)) {
+      history = await attic.get(ATTIC_KEY);
     }
-    
-    values.push(value);
-    await attic.set(args.ATTIC_KEY, values);
-    log.info(`Logged '${value}' from ${args.URL}`);
+
+    history.push(value);
+    await attic.set(ATTIC_KEY, history);
+    log.info(`Logged '${value}' from ${URL}`);
   } catch (e) {
-    log.error(`GET ${args.URL} ERROR`);
+    log.error(`GET ${URL} error:`);
     log.error(e);
   }
 };
