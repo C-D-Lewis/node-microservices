9869a237fce0faddd0754678627c96724ebb6e39
C-D-Lewis <Bonsitm@gmail.com>
2019-12-22 12:35:53 +0000

    Refactor upgrade.js

--- apps/conduit/src/api/upgrade.js
+++ apps/conduit/src/api/upgrade.js
@@ -1,21 +1,28 @@
 const { execSync } = require('child_process');
 
+/** Time until reboot. */
 const DELAY_MS = 10000;
 
-module.exports = async (req, res) => {
+/**
+ * Handle an API request to do a git upgrade.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
+const handleUpgradeRequest = async (req, res) => {
   try {
-    // Git update
     execSync('git checkout ../..');
     execSync('git reset --hard HEAD');
     execSync('git pull origin master');
 
-    // Delayed reboot
-    setTimeout(() => execSync('sudo reboot'), DELAY_MS);  
+    setTimeout(() => execSync('sudo reboot'), DELAY_MS);
 
-    res.status(200);
-    res.json({ content: 'Restarting in 10 seconds' });
+    res.status(200).json({ content: `Restarting in ${DELAY_MS / 1000} seconds` });
   } catch (e) {
-    log.error(e);
-    res.status(500);
+    const error = `Error performing upgrade: ${e.stack}`;
+    log.error(error);
+    res.status(500).send({ error });
   }
 };
+
+module.exports = handleUpgradeRequest;
