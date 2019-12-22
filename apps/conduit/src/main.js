039054e2a9c6edee3b82a8ba872da0eb30618370
C-D-Lewis <Bonsitm@gmail.com>
2019-12-22 13:00:01 +0000

    Refactor main.js

--- apps/conduit/src/main.js
+++ apps/conduit/src/main.js
@@ -1,13 +1,19 @@
 const { log } = require('./node-common')(['log']);
 const api = require('./modules/api');
-const fleet = require('./modules/fleet');
+const { checkIn } = require('./modules/fleet');
 
+/** Time to wait before updating the fleet registry */
+const FLEET_CHECKIN_DELAY_MS = 30000;
+
+/**
+ * The main function.
+ */
 const main = async () => {
   log.begin();
   api.setup();
 
   // Wait for Attic to come up
-  setTimeout(async () => await fleet.checkIn(), 30000);
+  setTimeout(checkIn, FLEET_CHECKIN_DELAY_MS);
 };
 
 main();
