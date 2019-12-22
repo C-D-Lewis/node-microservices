77cb64a2658635ebec25b84a8126054c9d066896
C-D-Lewis <Bonsitm@gmail.com>
2019-12-22 11:54:15 +0000

    Refactor apps.js

--- apps/conduit/src/api/apps.js
+++ apps/conduit/src/api/apps.js
@@ -2,25 +2,35 @@ const { config } = require('..//node-common')(['config']);
 const allocator = require('../modules/allocator');
 const util = require('../modules/util');
 
-module.exports = async (req, res) => {
-  const result = [];
+/**
+ * Get the status of an allocated app, and construct the response piece.
+ *
+ * @param {Object} item - Allocator item.
+ * @returns {Promise<Object>}
+ */
+const getAppStatus = async (item) => {
+  const res = await util.sendPacket({ to: item.app, topic: 'status' });
+  return {
+    ...item,
+    status: (res.message && res.message.content) ? res.message.content : JSON.stringify(res),
+  };
+};
+
+/**
+ * Collate status of all connected apps and response with a list.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
+const respondWithApps = async (req, res) => {
   const apps = allocator.getAll();
-
-  await Promise.all(apps.map(async (item) => {
-    const app = Object.assign({}, item);
-    const packet = await util.sendPacket({ to: item.app, topic: 'status' });
-
-    app.status = (packet.message && packet.message.content)
-      ? packet.message.content
-      : JSON.stringify(packet);
-
-    result.push(app);
-  }));
+  const result = await Promise.all(apps.map(getAppStatus));
 
   // Report conduit itself for completeness
   result.push({ app: 'conduit', port: config.SERVER.PORT, status: 'OK' });
 
-  res.status(200);
   res.set('Access-Control-Allow-Origin', '*');
-  res.send(JSON.stringify(result, null, 2));
+  res.status(200).send(JSON.stringify(result, null, 2));
 };
+
+module.exports = respondWithApps;
