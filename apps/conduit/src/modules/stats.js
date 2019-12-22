ab0771f0b40892fdcf570e2e17e0f712f47fa65a
C-D-Lewis <Bonsitm@gmail.com>
2019-12-22 12:53:35 +0000

    Deactivate stats.js

--- apps/conduit/src/modules/stats.js
+++ apps/conduit/src/modules/stats.js
@@ -1,66 +1,68 @@
-const { config, log, requestAsync } = require('../node-common')(['config', 'log', 'requestAsync']);
-const allocator = require('./allocator');
+// Currently unused, needs style update
 
-config.requireKeys('stats.js', {
-  required: ['OPTIONS'],
-  properties: {
-    OPTIONS: {
-      required: ['RECORD_STATS'],
-      properties: {
-        RECORD_STATS: { type: 'boolean' },
-      },
-    },
-  },
-});
+// const { config, log, requestAsync } = require('../node-common')(['config', 'log', 'requestAsync']);
+// const allocator = require('./allocator');
 
-// Can't use conduit.js - not a Conduit app!
-const atticSend = async (json) => {
-  const { body } = await requestAsync({
-    url: `http://localhost:${allocator.findByApp('attic').port}/conduit`,
-    method: 'post',
-    json,
-  });
+// config.requireKeys('stats.js', {
+//   required: ['OPTIONS'],
+//   properties: {
+//     OPTIONS: {
+//       required: ['RECORD_STATS'],
+//       properties: {
+//         RECORD_STATS: { type: 'boolean' },
+//       },
+//     },
+//   },
+// });
 
-  return body;
-};
+// // Can't use conduit.js - not a Conduit app!
+// const atticSend = async (json) => {
+//   const { body } = await requestAsync({
+//     url: `http://localhost:${allocator.findByApp('attic').port}/conduit`,
+//     method: 'post',
+//     json,
+//   });
 
-const recordPacket = async ({ to, topic, from }) => {
-  if (!config.OPTIONS.RECORD_STATS) {
-    return;
-  }
+//   return body;
+// };
 
-  let current = await atticSend({
-    to: 'attic', topic: 'get', message: { app: 'conduit', key: 'stats' },
-  });
+// const recordPacket = async ({ to, topic, from }) => {
+//   if (!config.OPTIONS.RECORD_STATS) {
+//     return;
+//   }
 
-  // No stats yet
-  if (current.error && current.status === 404) {
-    const initialState = { recipients: {}, senders: {}, topics: {}, total: 0 };
-    current = {
-      message: { value: initialState },
-    };
-  }
+//   let current = await atticSend({
+//     to: 'attic', topic: 'get', message: { app: 'conduit', key: 'stats' },
+//   });
 
-  // Compile the data update for Attic (can be initial state)
-  const update = Object.assign({}, current.message.value);
-  update.recipients[to] = update.recipients[to] || 0;
-  update.senders[from] = update.senders[from] || 0;
-  update.topics[topic] = update.topics[topic] || 0;
-  update.total = update.total || 0;
+//   // No stats yet
+//   if (current.error && current.status === 404) {
+//     const initialState = { recipients: {}, senders: {}, topics: {}, total: 0 };
+//     current = {
+//       message: { value: initialState },
+//     };
+//   }
 
-  // Update the state
-  update.recipients[to]++;
-  update.senders[from]++;
-  update.topics[topic]++;
-  update.total++;
+//   // Compile the data update for Attic (can be initial state)
+//   const update = Object.assign({}, current.message.value);
+//   update.recipients[to] = update.recipients[to] || 0;
+//   update.senders[from] = update.senders[from] || 0;
+//   update.topics[topic] = update.topics[topic] || 0;
+//   update.total = update.total || 0;
 
-  await atticSend({
-    to: 'attic',
-    topic: 'set',
-    message: { app: 'conduit', key: 'stats', value: update },
-  });
-};
+//   // Update the state
+//   update.recipients[to]++;
+//   update.senders[from]++;
+//   update.topics[topic]++;
+//   update.total++;
 
-module.exports = {
-  recordPacket,
-};
+//   await atticSend({
+//     to: 'attic',
+//     topic: 'set',
+//     message: { app: 'conduit', key: 'stats', value: update },
+//   });
+// };
+
+// module.exports = {
+//   recordPacket,
+// };
