3bd1dedff93f79ab96c589d6c53303ab6178a813
C-D-Lewis <Bonsitm@gmail.com>
2019-12-22 12:47:42 +0000

    Refactor api.js

--- apps/conduit/src/modules/api.js
+++ apps/conduit/src/modules/api.js
@@ -1,25 +1,29 @@
 const bodyParser = require('body-parser');
 const { server } = require('../node-common')(['server']);
-const allocator = require('./allocator');
 
 /**
  * Middleware to enable browser pre-flight requests.
  *
- * @param {object} req - Request object.
- * @param {object} res - Response object.
- * @param {function} next - Callback for next middleware.
+ * @param {Object} req - Request object.
+ * @param {Object} res - Response object.
+ * @param {Function} next - Callback for next middleware.
  */
 const enablePreflight = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 };
 
+/**
+ * Setup the API.
+ */
 const setup = () => {
   server.start();
 
   const app = server.getExpressApp();
   app.use(enablePreflight);
+
+  // Register API routes and handlers
   app.get('/apps', require('../api/apps'));
   app.post('/conduit', bodyParser.json(), require('../api/conduit'));
   app.get('/port', bodyParser.json(), require('../api/port'));
