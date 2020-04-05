85229a15e372f797baef78b3d5ad7df62294c441
C-D-Lewis <Bonsitm@gmail.com>
2020-04-05 14:21:36 +0100

    Add auth to show-fleet.sh

--- tools/show-fleet.sh
+++ tools/show-fleet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./conduit.sh 46.101.3.163 attic get '{"app": "conduit", "key": "fleetList"}' | jq '.message.value'
+./conduit.sh 46.101.3.163 attic get '{"app": "conduit", "key": "fleetList"}' $CONDUIT_TOKEN | jq '.message.value'
