b16d37e3f736c3cea3bf355f115c1d8380bb14e8
C-D-Lewis <Bonsitm@gmail.com>
2020-04-05 10:09:33 +0100

    Update conduit.sh for auth

--- tools/conduit.sh
+++ tools/conduit.sh
@@ -3,9 +3,14 @@
 # Example:
 #  ./conduit.sh 192.168.1.107 BacklightServer set '{"all": [128,128,128]}'
 
+# Authorization comes after payload, if requried
+#  ./conduit.sh localhost guestlist create '{"name": "test"...}' $adminPassword
+
 PORT=5959
 MESSAGE=${4:-"{}"}
-PACKET="{\"to\":\"$2\",\"topic\":\"$3\",\"message\":$MESSAGE}"
+AUTH=$5
+PACKET="{\"to\":\"$2\",\"topic\":\"$3\",\"message\":$MESSAGE,\"auth\":\"$AUTH\"}"
+echo $PACKET
 
 curl -X POST $1:$PORT/conduit -H Content-Type:application/json -d "$PACKET" -s
 echo ''
