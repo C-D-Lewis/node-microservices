98f2bf8bf6e33be97fac7917b9de206643ecac56
C-D-Lewis <Bonsitm@gmail.com>
2020-11-02 22:23:04 +0000

    Add mote-phat-fade.py

---
+++ node-common/src/lib/mote-phat-fade.py
@@ -0,0 +1,51 @@
+# python mote-phat-fade.py <fr> <fg> <fb> <tr> <tb> <tg>
+# Mirrors on all pixels on channels
+
+import platform
+import sys
+import time
+
+start_r = int(sys.argv[1])
+start_g = int(sys.argv[2])
+start_b = int(sys.argv[3])
+target_r = int(sys.argv[4])
+target_g = int(sys.argv[5])
+target_b = int(sys.argv[6])
+
+# Handle not running on Pi
+if 'arm' not in platform.machine():
+  print('Not running on ARM')
+  sys.exit(0)
+
+NUM_CHANNELS = 2
+NUM_PIXELS = 16
+INTERVAL = 0.l
+STEP = 5
+
+import motephat as mote
+
+def is_complete():
+  return start_r == target_r && start_g == target_g && start_b == target_g
+
+def main():
+  mote.set_clear_on_exit(False)  # Very important
+  for c in range(NUM_CHANNELS):
+    mote.configure_channel(c + 1, NUM_PIXELS, True)
+
+  current_r = start_r
+  current_g = start_g
+  current_b = start_b
+  while not is_complete():
+    mote.set_all(current_r, current_g, current_b)
+    mote.show()
+
+    diff = abs(current_r - target_r)
+    current_r += (STEP if diff > STEP else diff) * (-1 if current_r > target_r else 1)
+    diff = abs(current_g - target_g)
+    current_g += (STEP if diff > STEP else diff) * (-1 if current_g > target_g else 1)
+    diff = abs(current_b - target_b)
+    current_b += (STEP if diff > STEP else diff) * (-1 if current_b > target_b else 1)
+    time.sleep(INTERVAL)
+
+if '__main__' in __name__:
+  main()
--- node-common/src/modules/leds.js
+++ node-common/src/modules/leds.js
@@ -2,6 +2,7 @@ const os = require('os');
 const config = require('./config');
 const log = require('./log');
 
+/** Supported HATs and other LED hardware */
 const SUPPORTED_BOARDS = ['mote', 'blinkt'];
 
 config.requireKeys('leds.js', {
@@ -14,7 +15,6 @@ config.requireKeys('leds.js', {
         BRIGHTNESS: { type: 'number', maximum: 1.0 },
         USE_HARDWARE: { type: 'boolean' },
         HARDWARE_TYPE: { type: 'string', enum: SUPPORTED_BOARDS },
-        USE_SERVER: { type: 'boolean' },
       },
     },
   },
@@ -22,7 +22,9 @@ config.requireKeys('leds.js', {
 
 log.assert(SUPPORTED_BOARDS.includes(config.LEDS.HARDWARE_TYPE), 'Valid hardware type', true);
 
+/** Time between blink on and off */
 const BLINK_TIME_MS = 100;
+/** Number of LEDs depending on configured hardware */
 const NUM_LEDS = {
   mote: 16,
   blinkt: 8,
@@ -34,23 +36,29 @@ let mote = {};
 let initialised;
 let blinkHandle = null;
 
+/**
+ * Can LED hardware be used?
+ *
+ * @returns {boolean}
+ */
 const hardwareAvailable = () => {
-  if (!os.arch().includes('arm') || !config.LEDS.USE_HARDWARE) {
-    return false;
-  }
-
-  if (!initialised) {
-    init();
-  }
+  if (!os.arch().includes('arm') || !config.LEDS.USE_HARDWARE) return false;
+  if (!initialised) init();
 
   return true;
 };
 
+/**
+ * Update blinkt - library somehow requires two of these.
+ */
 const updateBlinkt = () => {
   blinkt.sendUpdate();
   blinkt.sendUpdate();
 };
 
+/**
+ * Init LED states and hardware library.
+ */
 const init = () => {
   for (let i = 0; i < NUM_LEDS; i++) {
     pixelsState.push([0, 0, 0]);
@@ -61,74 +69,74 @@ const init = () => {
     blinkt = new NodeBlinkt();
     blinkt.setup();
     blinkt.setAllPixels(0, 0, 0, 1.0);
-    updateBlinkt();
-  } else if (config.LEDS.HARDWARE_TYPE === 'mote') {
-    mote = require('./motePhat');
 
-    if (config.LEDS.USE_SERVER) {
-      // Use faster Python server to quicken fade intervals
-      // For some reason on both soldered boards this triggers a single LED
-      // When set to all black (0,0,0)
-      mote.startServer();
-    }
+    updateBlinkt();
+  }
+
+  if (config.LEDS.HARDWARE_TYPE === 'mote') {
+    mote = require('./motePhat');
   }
 
   initialised = true;
 };
 
+/**
+ * Set all LEDs to a new color.
+ *
+ * @param {Array<number>} nextRgb - RGB color values.
+ */
 const setAll = async (nextRgb) => {
   for (let i = 0; i < NUM_LEDS; i++) {
     pixelsState[i] = nextRgb;
   }
 
-  if (!hardwareAvailable()) {
-    return;
-  }
+  if (!hardwareAvailable()) return;
 
   if (config.LEDS.HARDWARE_TYPE === 'blinkt') {
-    for (let i = 0; i < NUM_LEDS; i++) {
-      set(i, nextRgb);
-    }
-  } else if (config.LEDS.HARDWARE_TYPE === 'mote') {
-    mote.setAll(nextRgb);
+    for (let i = 0; i < NUM_LEDS; i++) set(i, nextRgb);
+  }
 
-    if (config.LEDS.USE_SERVER) {
-      // Server implementation is faster, but has problems (see above)
-      await mote.setAllServer(nextRgb);
-    } else {
-      mote.setAll(nextRgb);
-    }
+  if (config.LEDS.HARDWARE_TYPE === 'mote') {
+    mote.setAll(nextRgb);
   }
 };
 
+/**
+ * Set a single LED to a new color.
+ *
+ * @param {number} index - LED index to change.
+ * @param {Array<number>} nextRgb - RGB color values.
+ */
 const set = (index, nextRgb) => {
   pixelsState[index] = nextRgb;
 
-  if (!hardwareAvailable()) {
-    return;
-  }
+  if (!hardwareAvailable()) return;
 
   if (config.LEDS.HARDWARE_TYPE === 'blinkt') {
     const rgb = nextRgb.map(p => p * config.LEDS.ATTENUATION_FACTOR);
     blinkt.setPixel(index, ...rgb, config.LEDS.BRIGHTNESS);
     updateBlinkt();
-  } else if (config.LEDS.HARDWARE_TYPE === 'mote') {
+  }
+
+  if (config.LEDS.HARDWARE_TYPE === 'mote') {
     mote.setPixels(pixelsState);
   }
 };
 
+/**
+ * Blink a single LED.
+ *
+ * @param {number} index - LED index to change.
+ * @param {Array<number>} nextRgb - RGB color values.
+ */
 const blink = (index, nextRgb) => {
   pixelsState[index] = nextRgb;
 
-  if (!hardwareAvailable()) {
-    return;
-  }
+  if (!hardwareAvailable()) return;
 
   // Allow rapid requests to persist the blink, but not cancel it
   set(index, nextRgb);
-  if (blinkHandle) {
-    return;
-  }
+  if (blinkHandle) return;
 
   blinkHandle = setTimeout(() => {
     set(index, [0, 0, 0]);
@@ -138,9 +146,7 @@ const blink = (index, nextRgb) => {
 
 // OK to init mote straight away - Non-pi dev should specify 'blinkt' HARDWARE_TYPE
 (() => {
-  if (config.LEDS.HARDWARE_TYPE === 'mote') {
-    init();
-  }
+  if (config.LEDS.HARDWARE_TYPE === 'mote') init();
 })();
 
 module.exports = {
