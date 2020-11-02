7b69f798f36239f989dff6ac2d5ef03d88095f55
C-D-Lewis <Bonsitm@gmail.com>
2020-11-02 22:57:15 +0000

    Update mote-phat-fade.py

--- node-common/src/lib/mote-phat-fade.py
+++ node-common/src/lib/mote-phat-fade.py
@@ -5,12 +5,8 @@ import platform
 import sys
 import time
 
-start_r = int(sys.argv[1])
-start_g = int(sys.argv[2])
-start_b = int(sys.argv[3])
-target_r = int(sys.argv[4])
-target_g = int(sys.argv[5])
-target_b = int(sys.argv[6])
+start = [int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])]
+target = [int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6])]
 
 # Handle not running on Pi
 if 'arm' not in platform.machine():
@@ -29,19 +25,21 @@ def main():
   for c in range(NUM_CHANNELS):
     mote.configure_channel(c + 1, NUM_PIXELS, True)
 
-  current_r = start_r
-  current_g = start_g
-  current_b = start_b
-  while (current_r != target_r and current_g != target_g and current_b != target_g):
-    mote.set_all(current_r, current_g, current_b)
+  current = start.copy()
+  print(start)
+  print(current)
+  print(target)
+
+  while current != target:
+    mote.set_all(current[0], current[1], current[2])
     mote.show()
 
-    diff = abs(current_r - target_r)
-    current_r += (STEP if diff > STEP else diff) * (-1 if current_r > target_r else 1)
-    diff = abs(current_g - target_g)
-    current_g += (STEP if diff > STEP else diff) * (-1 if current_g > target_g else 1)
-    diff = abs(current_b - target_b)
-    current_b += (STEP if diff > STEP else diff) * (-1 if current_b > target_b else 1)
+    diff = abs(current[0] - target[0])
+    current[0] += (STEP if diff > STEP else diff) * (-1 if current[0] > target[0] else 1)
+    diff = abs(current[1] - target[1])
+    current[1] += (STEP if diff > STEP else diff) * (-1 if current[1] > target[1] else 1)
+    diff = abs(current[2] - target[2])
+    current[2] += (STEP if diff > STEP else diff) * (-1 if current[2] > target[2] else 1)
     time.sleep(INTERVAL)
 
 if '__main__' in __name__:
