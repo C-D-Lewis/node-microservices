7fd365fe37f44597a6b7532489a4e8d46d6235f8
C-D-Lewis <Bonsitm@gmail.com>
2022-02-23 22:41:33 +0000

    Log module upgrades

--- node-common/src/modules/log.js
+++ node-common/src/modules/log.js
@@ -5,71 +5,66 @@ config.requireKeys('log.js', {
   required: ['LOG'],
   properties: {
     LOG: {
-      required: ['APP_NAME', 'LEVEL', 'TO_FILE'],
+      required: ['APP_NAME', 'LEVEL'],
       properties: {
         APP_NAME: { type: 'string' },
         LEVEL: { type: 'string' },
-        TO_FILE: { type: 'boolean' },
       },
     },
   },
 });
 
+/** Log start decor width */
 const DECOR_WIDTH = 80; // process.stdout.columns;
+/** Tag symbols */
 const TAGS = {
   info: 'I',
   debug: 'D',
   error: 'E',
   fatal: 'F',
 };
+/** Time between log size evaluations */
+const MONITOR_INTERVAL_MS = 60000;
+/** Max log size */
+const MAX_SIZE_MB = 50;
+/** Log path */
+const FILE_PATH = `${config.getInstallPath()}/${config.LOG.APP_NAME.split(' ').join('-')}.log`;
 
+/**
+ * Get a time string.
+ *
+ * @returns {string} Simple time string.
+ */
 const getTimeString = () => {
   const str = new Date().toISOString();
   return str.substring(str.indexOf('T') + 1, str.indexOf('.'));
 };
 
-const writePid = () => fs.writeFileSync(`${config.getInstallPath()}/pid`, `${process.pid}`, 'utf8');
-
+/**
+ * Write a log line to file.
+ *
+ * @param {string} msg - Message to write.
+ */
 const writeToFile = (msg) => {
-  const filePath = `${config.getInstallPath()}/${config.LOG.APP_NAME.split(' ').join('-')}.log`;
-
   let stream;
-  if (!fs.existsSync(filePath)) {
-    stream = fs.createWriteStream(filePath, { flags: 'w' });
+  if (!fs.existsSync(FILE_PATH)) {
+    stream = fs.createWriteStream(FILE_PATH, { flags: 'w' });
     stream.end(`[${getTimeString()}] New log file!\n`);
   }
 
-  stream = fs.createWriteStream(filePath, { flags: 'a' });
+  stream = fs.createWriteStream(FILE_PATH, { flags: 'a' });
   stream.end(`${msg}\n`);
 };
 
-const log = (level, msg) => {
-  // If the log level includes it, or its an important error
-  if (!(config.LOG.LEVEL.includes(level) || ['error', 'fatal'].includes(level))) {
-    return;
-  }
-
-  if (!assert(msg, `log 'msg' must not be undefined`, false)) {
-    throw new Error('log \'msg\' was undefined');
-  }
-
-  if (typeof msg === 'object') {
-    msg = msg.message || JSON.stringify(msg);
-  }
-
-  if (config.LOG.TO_FILE) {
-    writeToFile(msg);
-  }
-
-  console.log(`[${TAGS[level]} ${getTimeString()} ${process.pid} ${config.LOG.APP_NAME}] ${msg}`);
-
-  if (level === 'fatal') {
-    // Oof
-    process.exit(1);
-  }
-};
-
-const assert = (condition, msg, strict) => {
+/**
+ * Asset a condition, else log that it wasn't true.
+ *
+ * @param {boolean} condition - Condition statement to evaluate.
+ * @param {string} msg - Message explaining the assert.
+ * @param {boolean} strict - If a strict assertion, halt the app.
+ * @returns {boolean} The condition outcome.
+ */
+const assert = (condition, msg, strict = false) => {
   if (!condition) {
     msg = `Assertion failed: ${msg}`;
     const func = strict ? fatal : error;
@@ -79,7 +74,45 @@ const assert = (condition, msg, strict) => {
   return condition;
 };
 
-const writeDecor = () => {
+/**
+ * Log a message.
+ *
+ * @param {string} level - Log level.
+ * @param {string|object|Error} msg - Message to log.
+ * @returns {undefined}
+ */
+const log = (level, msg) => {
+  // If the log level includes it, or its an important error
+  if (!(config.LOG.LEVEL.includes(level) || ['error', 'fatal'].includes(level))) return;
+
+  // Message must be something
+  if (!assert(msg, `log 'msg' must not be undefined`, false))
+    throw new Error('log \'msg\' was undefined');
+
+  // Handle Error or JSON object
+  if (typeof msg === 'object') {
+    msg = msg.message || JSON.stringify(msg);
+  }
+
+  // Write to all outputs
+  const logLine = `[${TAGS[level]} ${getTimeString()} ${process.pid} ${config.LOG.APP_NAME}] ${msg}`;
+  writeToFile(logLine);
+  console.log(logLine);
+
+  // Fatal messages halt the app
+  if (level === 'fatal') process.exit(1);
+};
+
+// Helper aliases
+const info = msg => log('info', msg);
+const debug = msg => log('debug', msg);
+const error = msg => log('error', msg);
+const fatal = msg => log('fatal', msg);
+
+/**
+ * Print fancy decor when app starts.
+ */
+const printDecor = () => {
   const msg = ` ${config.LOG.APP_NAME} `;
   const decorLength = Math.floor((DECOR_WIDTH - msg.length) / 2);
   process.stdout.write('='.repeat(decorLength));
@@ -88,9 +121,30 @@ const writeDecor = () => {
   process.stdout.write('\n');
 };
 
+/**
+ * Monitor the log size, and erase if it gets too big.
+ */
+const monitorLogSize = () => {
+  setInterval(() => {
+    const { size } = fs.statSync(FILE_PATH);
+    const sizeMb = size / (1024*1024);
+    info(`Logfile size: ${Math.round(sizeMb * 100) / 100} MB`);
+
+    if (sizeMb < MAX_SIZE_MB) return;
+
+    // Erase the file and start again
+    fs.unlinkSync(FILE_PATH);
+    info('Log file exceeded max size and was restarted');
+  }, MONITOR_INTERVAL_MS);
+  info(`Monitoring logfile size`);
+};
+
+/**
+ * Begin app logging, and handle uncaught errors.
+ */
 const begin = () => {
-  writeDecor();
-  writePid();
+  printDecor();
+  monitorLogSize();
 
   process.on('uncaughtException', (err) => {
     error('uncaughtException:');
@@ -101,11 +155,6 @@ const begin = () => {
     error(err.stack);
   });
 };
-
-const info = msg => log('info', msg);
-const debug = msg => log('debug', msg);
-const error = msg => log('error', msg);
-const fatal = msg => log('fatal', msg);
 
 module.exports = {
   begin,
