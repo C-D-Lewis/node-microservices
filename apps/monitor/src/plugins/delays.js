38db12d61a361bc80132177b77fb0ddf9376d1f6
C-D-Lewis <bonsitm@gmail.com>
2025-09-18 09:47:24 +0100

    update

--- apps/monitor/src/plugins/delays.js
+++ apps/monitor/src/plugins/delays.js
@@ -12,7 +12,7 @@ const TUBE_LINES = ['jubilee'];
 /** Hours to alert */
 const HOURS = [7, 19];
 /** String to ignore */
-const IGNORE = ['Ely', 'Cambridge', 'Manningtree'];
+const IGNORE = ['Ely', 'Cambridge', 'Manningtree', 'Chingford', 'Seven Sisters', 'Tottenham Hale'];
 /** Fetch fixed options */
 const FETCH_OPTS = {
   headers: {
@@ -22,24 +22,51 @@ const FETCH_OPTS = {
 
 let alert;
 let latestText = '';
+let latestCount = 0;
+
+/**
+ * Fetch rought <li> list of distruptions from National Rail.
+ *
+ * @returns {string[]} <li> list.
+ */
+const fetchNationalRailList = async () => {
+  const url = 'https://www.nationalrail.co.uk/status-and-disruptions/?mode=train-operator-status';
+  const { body } = await fetch(url, FETCH_OPTS);
+
+  return extract(body, ['Disruptions on these'], 'Good service for all').split('<li>');
+};
+
+/**
+ * Fetch TfL APi data.
+ *
+ * @returns {object[]} List of API objects.
+ */
+const fetchTflList = async () => {
+  const url = `https://api.tfl.gov.uk/line/mode/${TFL_MODES.join(',')}/status`;
+  const { data } = await fetch(url);
+
+  return data;
+};
 
 /**
  * Check for delays on a line.
  *
+ * @param {string[]} list - List of <li> from the page.
  * @param {string} lineName - Line name to check.
  * @returns {boolean} true if the line is OK.
  */
-const checkNationalRail = async (lineName) => {
-  const url = 'https://www.nationalrail.co.uk/status-and-disruptions/?mode=train-operator-status';
-  const { body } = await fetch(url, FETCH_OPTS);
-
-  // Find first mention of operator in disruption section
-  const disruptions = extract(body, ['Disruptions on these'], 'Good service for all').split('<li>');
-  const found = disruptions.find((p) => p.includes(lineName));
+const checkNationalRailLine = (list, lineName) => {
+  const found = list.find((p) => p.includes(lineName));
   if (!found) return undefined;
 
   // Get the first label which seems to contain the description
   const description = extract(found, ['aria-label="'], '"');
+
+  // Check for things we don't care about
+  if (description && IGNORE.some((p) => description.includes(p))) {
+    log.debug(`Found some ignore text, skipping: ${description}`);
+    return undefined;
+  }
 
   // If mentioned at all, probably of interest
   return found ? (description || lineName) : undefined;
@@ -48,15 +75,14 @@ const checkNationalRail = async (lineName) => {
 /**
  * Check a TfL line.
  *
+ * @param {object[]} lines - API lines objects.
  * @param {string} lineId - ID of the line to check.
+ * @returns {string|undefined} Incident notice for this line if found.
  */
-const checkTflStatus = async (lineId) => {
-  const url = `https://api.tfl.gov.uk/line/mode/${TFL_MODES.join(',')}/status`;
-  const { data } = await fetch(url);
-
-  const line = data.find((p) => p.id === lineId);
+const checkTflLine = (lines, lineId) => {
+  const line = lines.find((p) => p.id === lineId);
   const disrupted = !line?.lineStatuses[0]?.statusSeverityDescription.includes('Good');
-  return disrupted ? lineId : undefined;
+  return disrupted ? line?.lineStatuses[0].reason : undefined;
 };
 
 /**
@@ -71,25 +97,28 @@ module.exports = async () => {
   alert = createAlert(
     'delays',
     async () => {
-      const rail = (await Promise.all(OPERATORS.map(checkNationalRail)))
+      // Fetch each only once
+      const nationalrailList = await fetchNationalRailList();
+      const tflList = await fetchTflList();
+
+      const railIncidents = OPERATORS
+        .map((p) => checkNationalRailLine(nationalrailList, p))
         .filter((p) => !!p);
-      const tfl = (await Promise.all(TUBE_LINES.map(checkTflStatus)))
+      const tflIncidents = TUBE_LINES
+        .map((p) => checkTflLine(tflList, p))
         .filter((p) => !!p);
-      log.debug({ rail, tfl });
+      log.debug(JSON.stringify({ railIncidents, tflIncidents }, null, 2));
 
       latestText = '';
-      if (rail.length) {
-        latestText += `Rail:
-${rail.join('\n')}
-`;
+      if (railIncidents.length) {
+        latestText += `Rail:\n${railIncidents.join('\n')}\n`;
       }
-      if (tfl.length) {
-        latestText += `TfL:
-${tfl.join('\n')}
-`;
+      if (tflIncidents.length) {
+        latestText += `TfL:\n${tflIncidents.join('\n')}\n`;
       }
 
-      return !(rail.length || tfl.length);
+      latestCount = railIncidents.length + tflIncidents.length;
+      return latestCount === 0;
     },
     (success) => (success
       ? 'No configured lines have incidents reported.'
