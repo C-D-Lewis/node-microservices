5e9003d5ad9ec0ee93bfae42e4b1ab944a7f3f1f
C-D-Lewis <bonsitm@gmail.com>
2026-09-01 21:39:34 +0100

    update

--- apps/conduit/src/modules/api.js
+++ apps/conduit/src/modules/api.js
@@ -10,6 +10,7 @@ const { server } = require('../node-common')(['server']);
  */
 const enablePreflight = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 };
